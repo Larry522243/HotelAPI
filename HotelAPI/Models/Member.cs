e0cf77ab7cf51791d4ccea438035c50ea6ed1618
Larry522243 <larry268782@gmail.com>
2022-11-04 00:15:23 +0800

    加入專案檔案。

---
+++ HotelAPI/Models/Member.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelAPI.Models
+{
+    public class Member
+    {
+        [Key]
+        public Guid Guid { get; set; }
+
+        [Required]
+        public string? FirstName { get; set; }
+
+        [Required]
+        public string? LastName { get; set; }
+
+        [Required]
+        public int Gender { get; set; }
+
+        [Required]
+        public DateTime Birth { get; set; }
+
+        [Required]
+        public string? Phone { get; set; }
+
+        [Required]
+        public string? Email { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+
+        [Required]
+        public string? IDNum { get; set; }
+
+        [Required]
+        public string? Country { get; set; }
+
+        [Required]
+        public string? City { get; set; }
+    }
+}
---
+++ HotelAPI/Models/RoomType.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelAPI.Models
+{
+    public class RoomType
+    {
+        [Key]
+        public int RTId { get; set; }
+
+        [Required]
+        public string? RTName { get; set; }
+
+        [Required]
+        public string? RTSquare { get; set; }
+
+
+    }
+}
